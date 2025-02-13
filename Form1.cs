b5e466cd1e09d9a80cb5338d1568b5d580136f2d
Valeriaalfaro05 <paulavaleria.alfarocoronado@unidemat.edu.mx>
2025-02-13 11:00:05 -0600

    Practica3
    area y perimetro de un circulo

--- Form1.cs
+++ Form1.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Grados
+namespace Practica3
 {
     public partial class Form1 : Form
     {
@@ -17,26 +19,54 @@ namespace Grados
             InitializeComponent();
         }
 
-        private void btnconvertir1_Click(object sender, EventArgs e)
+        private void btnarea_Click(object sender, EventArgs e)
         {
-            float cent = float.Parse(this.textBox1.Text);
-            float fair = (cent * 9 / 5) + 32;
-            this.convertir.Text = fair.ToString();
+            if
+                (double.TryParse(textBox2.Text, out
+                double radio
+                ))
+            {
+                double area = Math.PI * Math.Pow(radio, 2);
+                txtarea.Text = area.ToString("F2");
+            }
 
-
+            else
+            {
+                MessageBox.Show("ingrese un " +
+                    "numero valido para el radio.", "error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
         }
 
-        private void btnconvertir_Click(object sender, EventArgs e)
+        private void btnperi_Click(object sender, EventArgs e)
         {
-            float cent = float.Parse(this.textBox2.Text);
-            float fair = (cent -32) * 5/9;
-            this.convertir1.Text = fair.ToString();
+            if
+               (double.TryParse(textBox2.Text, out
+               double radio
+               ))
+            {
+                double perimetro = 2 * Math.PI * radio;
+                txtperi.Text = perimetro.ToString("F2");
+            }
 
+            else
+            {
+                MessageBox.Show("ingrese un " +
+                    "numero valido para el radio.", "error", MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
         }
 
-        private void CERRAR_Click(object sender, EventArgs e)
+        private void btncerrar_Click(object sender, EventArgs e)
         {
             this.Close();
         }
+
+        private void btnlimpiar_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+           
+        }
     }
+
 }
